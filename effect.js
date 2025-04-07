3c6c3c9a4a274bc43e90cec4545fb72db6ec7701
abhilash <amulaabhilash7754@gmail.com>
2025-04-07 21:51:13 +0530

    commit

--- effect.js
+++ effect.js
@@ -173,22 +173,19 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
-				});
-				
-			}
-			else{
-				msgLoop(i);
-			}			
-
-		});
-			// body...
+			$("p:nth-child("+i+")").fadeOut('slow').delay(1000).promise().done(function(){
+				i = i + 1;
+				$("p:nth-child("+i+")").fadeIn('slow').delay(20000); // ← increased display time here
+				if (i == 50) {
+					$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+						$('.cake').fadeIn('fast');
+					});
+				} else {
+					msgLoop(i);
+				}
+			});
 		}
+		
 		
 		msgLoop(0);
 		
